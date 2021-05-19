a48fbf2f904d05113fd4f2a0c946437c48653805
finischer <niklasfischer0@gmail.com>
2021-05-19 21:30:29 +0200

    Fix spellings mistake

--- src/components/Lists.js
+++ src/components/Lists.js
@@ -6,9 +6,9 @@ export default class Lists extends Component {
         return (
             <div className="lists-mylists">
                 <div className="container">
-                   <MyListsItem name="Einkaufliste #1" amount="3" />
-                   <MyListsItem name="Einkaufliste #2" amount="8" />
-                   <MyListsItem name="Einkaufliste #3" amount="10" />
+                   <MyListsItem name="Einkaufsliste #1" amount="3" />
+                   <MyListsItem name="Einkaufsliste #2" amount="8" />
+                   <MyListsItem name="Einkaufsliste #3" amount="10" />
                 </div>
             </div>
         )
