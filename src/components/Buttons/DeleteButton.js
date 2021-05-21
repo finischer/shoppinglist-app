276bf8ec39486e2983604b68929aa3422fa1b2a8
finischer <niklasfischer0@gmail.com>
2021-05-22 00:28:35 +0200

    -

--- src/components/Buttons/DeleteButton.js
+++ src/components/Buttons/DeleteButton.js
@@ -3,9 +3,7 @@ import React, { Component } from 'react'
 export default class DeleteButton extends Component {
     render() {
         return (
-            <div>
                 <button className="btn btn-delete"> {this.props.name} </button>
-            </div>
         )
     }
 }
