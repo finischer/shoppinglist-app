75a83de4bf899575a92c9483a3267bcfd18f4976
finischer <niklasfischer0@gmail.com>
2021-05-23 13:25:25 +0200

    Connect to Signup-API

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -5,20 +5,20 @@ import {Link} from 'react-router-dom';
 export default class Auth extends Component {
     render() {
         return (
-            <div className="container login">
+            <div className="container form-container">
                 <form>
-                    <label htmlFor="email"><b>E-Mail</b></label>
+
+                    <label htmlFor="email"><b>E-Mail*</b></label>
                     <input type="text" placeholder="E-Mail eingeben" name="email"  required />
 
-                    <label htmlFor="password"><b>Password</b></label>
-                    <input type="password" placeholder="Passwort eingeben" name="password" required />    
+                    <label htmlFor="password"><b>Password*</b></label>
+                    <input type="password" placeholder="Passwort eingeben" name="password" required />
 
-                    <button className="btn btn-login" type="submit">Login</button>
-                    <p id="signup-link"> <Link to="/signup">Registrieren</Link></p>
+
+                    <button className="btn btn-signup" type="submit">Login</button>
+                    <p id="login-link"> <Link to="/signup">Registrieren</Link></p>
                     
                 </form>
-
-                
             </div>
         )
     }
--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,24 +1,67 @@
 import React, { Component } from 'react'
 import {Link} from 'react-router-dom';
+import axios from 'axios';
+
+
 
 export default class Signup extends Component {
+    constructor(props){
+        super(props);
+        this.signUp = this.signUp.bind(this);
+    }
+
+
+    signUp(){
+
+        const vorname = document.getElementById('vorname').value
+        const email = document.getElementById('email').value
+        const passwort = document.getElementById('passwort').value
+        
+        const new_user = {
+            vorname,
+            email,
+            passwort
+        };
+        
+        axios({
+            method: 'post',
+            url: 'http://127.0.0.1:5000/signup',
+            data: new_user
+        })
+        .then( response => {
+            console.log(response)
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+
+    }
+
+    
     render() {
+        
+        
         return (
-            <div className="container signup">
-            <form>
+            <div className="container form-container">
+            <form id="signup-form">
+                <label htmlFor="vorname"><b>Vorname*</b></label>
+                <input id="vorname" type="text" placeholder="Vornamen eingeben" name="vorname"  required />
+
                 <label htmlFor="email"><b>E-Mail*</b></label>
-                <input type="text" placeholder="E-Mail eingeben" name="email"  required />
+                <input id="email" type="text" placeholder="E-Mail eingeben" name="email"  required />
 
                 <label htmlFor="password"><b>Password*</b></label>
-                <input type="password" placeholder="Passwort eingeben" name="password" required />
+                <input id="passwort" type="password" placeholder="Passwort eingeben" name="passwort" required />
 
                 <label htmlFor="password"><b>Password wiederholen*</b></label>
-                <input type="password" placeholder="Passwort wiederholen" name="password-repeat" required />     
+                <input id="password-repeat" type="password" placeholder="Passwort wiederholen" name="passwort-repeat" required />     
 
-                <button className="btn btn-signup" type="submit">Registrieren</button>
-                <p>Bereits ein Account? <br /> <Link to="/login">Einloggen</Link></p>
+                <button className="btn btn-signup" type="submit" onClick={this.signUp}>Registrieren</button>
+                <p id="login-link">Bereits ein Account? <br /> <Link to="/login">Einloggen</Link></p>
                 
             </form>
+
+            
         </div>
         )
     }
